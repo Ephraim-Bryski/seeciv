ecc0537aa4bd4812d42e1ce6b1d3bf2f2625957b
Ephraim-Bryski <38594736+Ephraim-Bryski@users.noreply.github.com>
2020-12-18 02:18:07 -0500

    Update change_under_text.js
    added optional input of parent div

--- change_under_text.js
+++ change_under_text.js
@@ -1,12 +1,13 @@
 // function to modify underbrace text:
-function changeUnderText(underTextIndex,value,numOfDec){
+function changeUnderText(underTextIndex,value,numOfDec,parent=document){
     // underTextIndex: underbrace you want to modify, input 1 for the first underbrace
     // value: value you want for underbrace 
     // numOfDec: number of decimals you want displayed
+    // parent: the div to search for the underbraces, optional 
 
 
     // get the element with the desired text:
-    textSpan=document.getElementsByClassName("mjx-under")[(underTextIndex-1)*2+1].children[0].children[0].children[0].children[0]
+    textSpan=parent.getElementsByClassName("mjx-under")[(underTextIndex-1)*2+1].children[0].children[0].children[0].children[0]
     
     // round the value:
     roundedValue=Math.round(value*(10**numOfDec))/(10**numOfDec)
