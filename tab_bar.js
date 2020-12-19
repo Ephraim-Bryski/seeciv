b580514b6ce499e42e4ae2452c8c988c83407de4
Ephraim-Bryski <38594736+Ephraim-Bryski@users.noreply.github.com>
2020-12-19 01:35:13 -0500

    Update tab_bar.js

--- tab_bar.js
+++ tab_bar.js
@@ -9,7 +9,7 @@ function makeTabBar(parent){
         var tab=document.createElement("a")
         tab.href=tabLinks[i]
         if (i==0){
-            tab.href="index.html"
+            tab.href="seeciv.com"
         }
         tab.innerHTML=tabTitles[i]
         tab.classList="tab"
