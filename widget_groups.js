32365cfdaa1754733c5f8ff5f900972f57462f32
Ephraim-Bryski <38594736+Ephraim-Bryski@users.noreply.github.com>
2021-01-27 20:45:39 -0500

    lots of updates

--- widget_groups.js
+++ widget_groups.js
@@ -1,4 +1,4 @@
-function  makeSliderGroup(parent,xRel,yRel,labelText,length,orientation,range,initialVal,updateFunction,thumbColor="#333",makeSlider=true,stepSpinner=1){    // xRel and yRel should be strings with the percentage relative position in parent from left and top
+function  makeSliderGroup(parent,xRel,yRel,labelText,length,orientation,range,initialVal,updateFunction,thumbColor="#333",makeSlider=true,stepSpinner=1,disable=false){    // xRel and yRel should be strings with the percentage relative position in parent from left and top
    //! add range as input
    // If I start wanting to add more optional inputs, I could just make a class and make them attributes
     var width=30                                                                        // just adjust to get the slider in a nice position
@@ -59,11 +59,12 @@ function  makeSliderGroup(parent,xRel,yRel,labelText,length,orientation,range,in
         if (this.value>range[1]){this.value=range[1]}
         updateInputs(this.value)
     }
+    spinnerElement.disabled=disable
     Object.assign(spinnerElement.style,{
         position: "static",
         width: "50px",
         transform: moveSpinnerString,
-        display: displayType
+        display: displayType,
     })
 
 
